81fc43c9d6109279d488d87f0b3ca11ff74a3dc3
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-07-14 15:42:38 +0000

    servizio spotify iniettato app-component

--- first-routed-app/src/app/app.component.ts
+++ first-routed-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SpotifyService } from './spotify.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-routed-app';
+  obsTrack : Observable<Object>;
+  // faccio iniettare lo spotify service e faccio una ricerca
+  constructor(public spotify : SpotifyService){
+    this.obsTrack = spotify.searchTrack("Take 4");
+    this.obsTrack.subscribe((data)=>console.log(data)); //visualizzo la ricerca sulla console
+  } 
 }
