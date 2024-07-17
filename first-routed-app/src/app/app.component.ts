880b94525c30581704410dfabf08fd1ea8a03998
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-07-17 10:21:53 +0000

    ricerca avanzata titolo canzone

--- first-routed-app/src/app/app.component.ts
+++ first-routed-app/src/app/app.component.ts
@@ -8,11 +8,31 @@ import { SpotifyService } from './spotify.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-routed-app';
+  /*title = 'first-routed-app';
   obsTrack : Observable<Object>;
+
   // faccio iniettare lo spotify service e faccio una ricerca
+
   constructor(public spotify : SpotifyService){
     this.obsTrack = spotify.searchTrack("Take 4");
     this.obsTrack.subscribe((data)=>console.log(data)); //visualizzo la ricerca sulla console
-  } 
+  }*/
+  query !: string;
+  title = 'first-routed-app';
+  obsTrack !: Observable<Object>;
+  results: any;
+  // faccio iniettare lo spotify service e faccio una ricerca
+  constructor(public spotify: SpotifyService) {
+    
+  }
+  //il metodo submit serve per poter fare una ricerca di una canzone attraverso un input nella pagina html
+  submit(query:HTMLInputElement): void {
+      if (!query.value) {
+        return;
+      }
+      this.query = query.value;
+      this.obsTrack = this.spotify.searchTrack(this.query);  //l'obs richiama il metodo searchTrack dello spotify service
+      this.obsTrack.subscribe((data) => this.results = data); 
+  }
+  
 }
--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -14,7 +14,7 @@ export class SpotifyService {
       const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
       const headers = new HttpHeaders({
         Authorization:
-          'Bearer BQBXHvNwQ53sbwd2vBWyth7jopIsJS9V4yLWY-8CeLKu_xWcLrmlMpS3TqGTo4w2noZDm2g-sI2ZulzE8WOU1mEB4PUDQhgh8G31i1Vv5s_ON0c6zS7EMtJYcQJ11wGwXGHOsSo0jqQz3xFCKxxmRZwNtW-3IHFCTo4aC_iyVdm6JF4MpGaryfezwlyC5MGn9OC_4jVcmm1CzCcSoRDqCs-0GQr9Uh2_Tm3T7fpmspFYp-WZ-0E1JsilxpC28E6EshxH7evaS9fVjld_RU76njjftd1LtexcFIwNy8GIvSPYDC_-CHj6PT0GaWFjJ71Q1kz588e2JA'
+          'Bearer BQBroseB4HO35nV6R-0PsvXeewwFHAX45tCDzZ2LPIenZpLIYLmlQPfIH6dgN2cXqFgsYTZxhIUe_BZr_W82GCZxdV-ATnZ6ri3MtgOkT7-Mcl14kBYOdMuyKxA0GbAvJmK_QExdGi8-bYSUJDaWawcIcwN4VmJ6RFSViVdVsYSub6uKVpcTS-wIR28-xVISuhT-9TBMyqVo-lPWEufiWQwbNdl_vT8ceBCKPgxlDNv23nCtgi63_uE_Oe6Mhh-Lj1vqVwJiwbUhvJoF_N1bek5l1qAl8up3eDgCgI6-vzZ1qkamV-mRZxdvdRdvT9tWR2HI2bKdFw'
       });
 
       let obsTracks = this.http.get(url, { headers });
