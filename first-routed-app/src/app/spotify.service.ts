b97ab29854977874661e77766b38eb9edd6b213e
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-07-18 14:38:58 +0000

    track component parte_1

--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -21,5 +21,14 @@ export class SpotifyService {
       return obsTracks;
   //Ritorno un observable ai componenti che richiedono il servizio
     }
+    getTrack(id: string) {
+      const url = `https://api.spotify.com/v1/tracks/${id}`;
+      const headers = new HttpHeaders({
+        Authorization:
+          'Bearer BQDhJAp6uUh9rIWs5WBZfCI7VUn0OtDdTvdtIOvqn5OCNAuL-RIZIP62flIodJdRX1TsS8zcGqFeOL3B5Ipx9a78_M2tuJR5wZN1oSkgwDdXYPhvoKZT4BoSyNgPiQHawbAGVd4bIfFQ5tBbxLlNXRFUptIsl8Br9FpaI2vLL6WMpuc8nP3rtb_hl_7mdP_gHOH0P6eUNBF69nvltr0bYhEvYY6tP7P56hFyzPE3Os-3TGfes_9hPpRpAha9Y5lcjJLGK8bCujlIbBIgZ4HAYd7Xst8t1qo4wN5ayemkDV5n16VD_NEELsuweky1G41DY7UF1bmv4Q'
+      });
+      
+      return this.http.get(url, { headers });
+    }
 
 }
