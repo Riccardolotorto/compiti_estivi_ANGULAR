8fbbb1d590105041491259935ddb1113e62614aa
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-08-25 14:19:33 +0000

    search component

--- 02.1.2-lotorto/src/app/app-routing.module.ts
+++ 02.1.2-lotorto/src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { ProductComponent } from './product/product.component';
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
   {path: 'search', component: SearchComponent},
-  {path: 'products/:id', component: ProductComponent}
+  {path: 'products/:id', component: ProductComponent},
+  {path: "", redirectTo: "/search", pathMatch: "full"} //path che si apre di default e risulta come il path home
 ];
 
 @NgModule({
--- 02.1.2-lotorto/src/app/search/search.component.ts
+++ 02.1.2-lotorto/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FoodService } from '../food.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  results: any;
+  query!: string;
+  size!: string;
+  obs!: Observable<object>;
 
+  constructor(public food: FoodService) {}
+
+  submit(query: HTMLInputElement, size: HTMLInputElement) {
+    if (!query.value || !size.value) {
+      return
+    }
+    this.query = query.value;
+    this.size = size.value;
+    this.obs = this.food.searchProduct(this.query, this.size);
+    this.obs.subscribe((data) => {this.results = data; console.log(this.results)});
+  }
+  renderResults(res: any): void {
+    this.results = null;
+    if (res && res.products && res.products.items) {
+      this.results = res.products.items;
+    }
+  }
 }
