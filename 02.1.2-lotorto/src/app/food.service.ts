249c4a2876a56e5f7e6820f77493c40352693fab
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-08-25 14:00:04 +0000

    funzione searchProduct

--- 02.1.2-lotorto/src/app/food.service.ts
+++ 02.1.2-lotorto/src/app/food.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  //questo metodo fa una ricerca sull'api in base alla query e alla size ricevuta (in questo caso sarà dall'utente)
+  searchProduct(query: string, size: string) {
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=${size}&json=true`;
+    return this.http.get(url);
+  }
 }
