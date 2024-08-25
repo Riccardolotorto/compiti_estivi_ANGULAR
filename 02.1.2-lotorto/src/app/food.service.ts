58500417699e7cb9f86ad5aef749428ce2a1e168
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-08-25 14:32:52 +0000

    completo

--- 02.1.2-lotorto/src/app/food.service.ts
+++ 02.1.2-lotorto/src/app/food.service.ts
@@ -14,4 +14,8 @@ export class FoodService {
     const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=${size}&json=true`;
     return this.http.get(url);
   }
+  getProduct(id: string) {
+    const url = `https://world.openfoodfacts.org/api/v0/product/${id}`;
+    return this.http.get(url); 
+  }
 }
--- 02.1.2-lotorto/src/app/product/product.component.ts
+++ 02.1.2-lotorto/src/app/product/product.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { FoodService } from '../food.service';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  product: any;
+  obsProduct!: Observable<ParamMap>;
+  obsFoodService!: Observable<Object>;
 
+  constructor(
+    private route: ActivatedRoute,
+    private food: FoodService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.obsProduct = this.route.paramMap;  //memorizza i parametri dell'url (in questo caso l'id del prodotto)
+    this.obsProduct.subscribe(this.getRouterParam);
+  }
+  getRouterParam = (params: ParamMap) => {
+    let prId = params.get('id');
+    console.log(prId);
+    this.obsFoodService = this.food.getProduct(String(prId));   //cerca il prodotto specifico in base all'id
+    this.obsFoodService.subscribe((data)=> this.product = data);
+  }
+  back() {
+    this.location.back()
+  }
 }
